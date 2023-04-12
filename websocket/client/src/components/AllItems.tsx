6b5d4e068ed80f7ababbbdaca97f10ebfcbfc814
Ben Rogerson <info@benrogerson.dev>
2023-04-12 19:23:45 +0930

    Casing adjustment

---
+++ websocket/client/src/components/AllItems.tsx
@@ -0,0 +1,43 @@
+import { Link, NavLink } from "react-router-dom";
+import { getErrorMessage } from "@/utils/getErrorMessage";
+import { useAllFlora } from "@/utils/hooks";
+
+function AllItems() {
+  const allFlora = useAllFlora();
+
+  if (allFlora.isLoading) return <>Loading</>;
+  if (allFlora.error)
+    return (
+      <div className="text-red-500">
+        Error: {getErrorMessage(allFlora.error)}
+      </div>
+    );
+
+  if (!allFlora.data) return null;
+
+  if (allFlora.data.length === 0)
+    return (
+      <div>
+        No flora added, <Link to="/add">add one now &rsaquo;</Link>
+      </div>
+    );
+
+  return (
+    <>
+      <ul
+        className={`grid gap-y-3 transition-opacity${
+          allFlora.isRefetching ? " opacity-50" : ""
+        }`}
+      >
+        {[...allFlora.data].reverse().map((flora) => (
+          <li key={flora.id} className="text-2xl">
+            <Link to={`/${flora.id}`}>{flora.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <NavLink to="/add">+ Add</NavLink>
+    </>
+  );
+}
+
+export default AllItems;
